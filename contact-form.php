c8ec72f997a751e216457fdf6541223cc87a2406
Gurneet <gurneet.me@gmail.com>
2020-06-26 10:03:37 -0400

    if in php added

--- contact-form.php
+++ contact-form.php
@@ -1,4 +1,5 @@
 <?php
+if (isset($_POST['submit'])){
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_post['phone']
@@ -15,5 +16,5 @@ $to = 'gurneet.me@gmail.com';
 $headers = "From: $email_from \r\n";
 mail($to,$email_subject,$email_body,$headers);
 header("Location: index.html");
-
+}
 ?>
